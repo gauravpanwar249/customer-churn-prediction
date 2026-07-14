718b945a764243a282ce3356c79d78bb93c5c8b3
Gaurav <pnwrgaurav82@gmail.com>
2026-07-14 11:32:54 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load model
+model = joblib.load("models/churn_xgboost.pkl")
+
+st.set_page_config(page_title="Customer Churn Prediction", layout="centered")
+
+st.title(" Customer Churn Prediction")
+
+st.write("Fill customer details and click Predict.")
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
+Partner = st.selectbox("Partner", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["Yes", "No"])
+
+tenure = st.number_input("Tenure", 0, 100, 12)
+
+PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
+
+MultipleLines = st.selectbox(
+    "Multiple Lines",
+    ["Yes", "No", "No phone service"]
+)
+
+InternetService = st.selectbox(
+    "Internet Service",
+    ["DSL", "Fiber optic", "No"]
+)
+
+OnlineSecurity = st.selectbox(
+    "Online Security",
+    ["Yes", "No", "No internet service"]
+)
+
+OnlineBackup = st.selectbox(
+    "Online Backup",
+    ["Yes", "No", "No internet service"]
+)
+
+DeviceProtection = st.selectbox(
+    "Device Protection",
+    ["Yes", "No", "No internet service"]
+)
+
+TechSupport = st.selectbox(
+    "Tech Support",
+    ["Yes", "No", "No internet service"]
+)
+
+StreamingTV = st.selectbox(
+    "Streaming TV",
+    ["Yes", "No", "No internet service"]
+)
+
+StreamingMovies = st.selectbox(
+    "Streaming Movies",
+    ["Yes", "No", "No internet service"]
+)
+
+Contract = st.selectbox(
+    "Contract",
+    ["Month-to-month", "One year", "Two year"]
+)
+
+PaperlessBilling = st.selectbox(
+    "Paperless Billing",
+    ["Yes", "No"]
+)
+
+PaymentMethod = st.selectbox(
+    "Payment Method",
+    [
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+)
+
+MonthlyCharges = st.number_input(
+    "Monthly Charges",
+    0.0,
+    200.0,
+    70.0
+)
+
+TotalCharges = st.number_input(
+    "Total Charges",
+    0.0,
+    10000.0,
+    1000.0
+)
+
+
+input_df = pd.DataFrame({
+
+    "gender":[gender],
+    "SeniorCitizen":[SeniorCitizen],
+    "Partner":[Partner],
+    "Dependents":[Dependents],
+    "tenure":[tenure],
+    "PhoneService":[PhoneService],
+    "MultipleLines":[MultipleLines],
+    "InternetService":[InternetService],
+    "OnlineSecurity":[OnlineSecurity],
+    "OnlineBackup":[OnlineBackup],
+    "DeviceProtection":[DeviceProtection],
+    "TechSupport":[TechSupport],
+    "StreamingTV":[StreamingTV],
+    "StreamingMovies":[StreamingMovies],
+    "Contract":[Contract],
+    "PaperlessBilling":[PaperlessBilling],
+    "PaymentMethod":[PaymentMethod],
+    "MonthlyCharges":[MonthlyCharges],
+    "TotalCharges":[TotalCharges]
+
+})
+
+
+if st.button("Predict"):
+
+    prediction = model.predict(input_df)
+
+    if prediction[0] == 1:
+        st.error("⚠ Customer is likely to Churn")
+    else:
+        st.success("Customer is likely to Stay")
